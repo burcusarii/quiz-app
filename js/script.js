e35e90d35766ef57730989c3c0a1d63025d339cd
burcu irem sarı <burcu.sari5334@gmail.com>
2023-02-05 17:11:53 +0300

    cevaplama seçenekleri güncellendi.
    sadece tek bir cevabın işaretlenebilmesi sağlandı. İşaretlenen cevabının doğru ve yanlış olma durumuna göre icon eklendi.

--- js/script.js
+++ js/script.js
@@ -47,6 +47,9 @@ document.querySelector(".btn-start").addEventListener("click", function () {
 const quiz_content = document.querySelector(".quiz-content");
 const quiz_answer = document.querySelector(".answer");
 
+const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
+const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
+
 document.querySelector(".next_btn").addEventListener("click", function () {
   if (quiz.sorular.length != quiz.soruIndex + 1) {
     quiz_content.innerHTML = " "; // daha önce eklenen sorunun html elemanlarının silinmesi için content ve answer elemanlarının için sıfırlanır.
@@ -69,27 +72,35 @@ function soruGoster(soru) {
   // cevap secenekleri
   for (let cevap in soru.cevapSecenekleri) {
     let option_b = document.createElement("span"); // a, b, c secenekleri için span.
-    option_b.classList.add("options");
+    option_b.classList.add("option");
     option_b.innerText = cevap; // a, b, c secenekleri span içine yazıldı.
     let option = document.createElement("span"); // seçenekler için span.
     option.innerText = soru.cevapSecenekleri[cevap]; // secenekler a, b, c yanına yazıldı.
+    let option_item = document.createElement("div");
     let options_div = document.createElement("div"); // opsiyonlar için div.
-    options_div.classList.add("option");
+    options_div.classList.add("options");
 
-    options_div.appendChild(option_b);
-    options_div.appendChild(option);
+    option_item.appendChild(option_b);
+    option_item.appendChild(option);
+    options_div.appendChild(option_item);
     quiz_answer.appendChild(options_div);
   }
 
-  const option = quiz_answer.querySelectorAll(".option");
-  console.log(option);
-
+  const option = quiz_answer.querySelectorAll(".options");
   for (let opt of option) {
     opt.addEventListener("click", function () {
       cevap = opt.querySelector("span").textContent;
       if (soru.cevabiKontrolEt(cevap)) {
         opt.classList.add("correct");
-      } else opt.classList.add("incorrect");
+        opt.insertAdjacentHTML("beforeend", correctIcon);
+      } else {
+        opt.classList.add("incorrect");
+        opt.insertAdjacentHTML("beforeend", incorrectIcon);
+      }
+      for (let i = 0; i < option.length; i++) {
+        console.log(option);
+        option[i].classList.add("disabled");
+      }
     });
   }
 }
